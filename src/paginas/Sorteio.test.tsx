60c1d649a45a8e764e5a9842e2b6585c3ebd97ce
Luciana <luciana.lucio@g.globo>
2023-03-17 11:32:34 -0300

    feat: create useResultadoSorteio hook

--- src/paginas/Sorteio.test.tsx
+++ src/paginas/Sorteio.test.tsx
@@ -1,12 +1,19 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import React from "react";
 import { RecoilRoot } from "recoil";
 import { useListaDeParticipantes } from "../state/hook/useListaDeParticipantes";
+import { useResultadoSorteio } from "../state/hook/useResultadoSorteio";
 import Sorteio from "./Sorteio";
 
 jest.mock('../state/hook/useListaDeParticipantes', () => {
     return {
         useListaDeParticipantes: jest.fn()
+    }
+})
+
+jest.mock('../state/hook/useResultadoSorteio', () => {
+    return {
+        useResultadoSorteio: jest.fn()
     }
 })
 
@@ -16,8 +23,15 @@ describe('na pagina de sorteio', () => {
         'Lucio',
         'Andreia'
     ]
+    const resultado = new Map([
+        ['Carlos', 'Andreia'],
+        ['Andreia', 'Lucio'],
+        ['Lucio', 'Carlos']
+    ])
+
     beforeEach(() => {
-        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes)
+        (useListaDeParticipantes as jest.Mock).mockReturnValue(participantes);
+        (useResultadoSorteio as jest.Mock).mockReturnValue(resultado);
     })
 
 
@@ -28,5 +42,25 @@ describe('na pagina de sorteio', () => {
 
         const opcoes = screen.queryAllByRole('option')
         expect(opcoes).toHaveLength(participantes.length)
+    })
+
+    test('o amigo secreto é exibido quando solicitado', () => {
+        render(<RecoilRoot>
+            <Sorteio />
+        </RecoilRoot>)
+
+        const select = screen.getByPlaceholderText('Selecione o seu nome')
+
+        fireEvent.change(select, {
+            target: {
+                value: participantes[0]
+            }
+        })
+
+        const botao = screen.getByRole('button')
+        fireEvent.click(botao)
+
+        const amigoSecreto = screen.getByRole('alert')
+        expect(amigoSecreto).toBeInTheDocument()
     })
 })
